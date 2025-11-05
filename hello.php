f7e3cdd8a646a32a55c64741b1adf2ae49489681
Raneem alhorany <114341515+RaneemAlhorany@users.noreply.github.com>
2025-11-05 06:39:36 +0300

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>FIRST FILE of php</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo "hi";
+            ?>
+        </h1>
+
+        <p>
+            Current date and time: 
+            <?php
+                echo date("d-m-Y H:i:s");
+            ?>
+        </p>
+    </body>
+</html>
